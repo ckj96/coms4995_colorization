baf3f6fa560552d45e3a737eea8e22e8edb2d97d
root <root@instance-1.c.colooooorization.internal>
2018-11-11 22:00:10 +0000

    add colorization

---
+++ colorization/colorization/colorize.sh
@@ -0,0 +1,3 @@
+INPUT_IMG = /home/cc4192/colorr/deep-photo-styletransfer-tf/examples/input/in8.png
+OUTPUT_IMG = /home/yanbeipang/colorization/colorization/results/in8_result.png
+python ./colorize.py -img_in $INPUT_IMG -img_out $OUTPUT_IMG
---
+++ colorization/colorize.sh
@@ -0,0 +1,2 @@
+INPUT_IMG=
+python ./colorize.py -img_in [[INPUT_IMG_PATH]] -img_out [[OUTPUT_IMG_PATH]]
