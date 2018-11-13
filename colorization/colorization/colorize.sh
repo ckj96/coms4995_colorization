3195d48ee51bb3f661357e4278998befe9e7b101
庞艳蓓 <pangyanbei@dyn-160-39-221-33.dyn.columbia.edu>
2018-11-13 13:59:11 -0500

    d

--- colorization/colorization/colorize.sh
+++
@@ -1,3 +0,0 @@
-INPUT_IMG = /home/cc4192/colorr/deep-photo-styletransfer-tf/examples/input/in8.png
-OUTPUT_IMG = /home/yanbeipang/colorization/colorization/results/in8_result.png
-python ./colorize.py -img_in $INPUT_IMG -img_out $OUTPUT_IMG
--- colorization/colorize.sh
+++
@@ -1,2 +0,0 @@
-INPUT_IMG=
-python ./colorize.py -img_in [[INPUT_IMG_PATH]] -img_out [[OUTPUT_IMG_PATH]]
