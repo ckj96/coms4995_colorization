c09bd3ca3ae02376f78ed87d8f058e60ed1afad7
庞艳蓓 <pangyanbei@dyn-160-39-220-177.dyn.columbia.edu>
2018-12-03 23:31:39 -0500

    changed names

--- colorization_no_model/colorization/colorize.sh
+++
@@ -1,8 +0,0 @@
-
- for file in input/*.png;
- do python ./colorize.py -img_in input/${file##*/} -img_out output/${file##*/};
-#do echo ${file##*/};
- done
-
-#IMG=building
-#python ./colorize.py -img_in input/$IMG.png -img_out output/$IMG.png
